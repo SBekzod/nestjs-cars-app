3206d837e7863bdcc485cc5b2d24ed0b37fe5af0
Martin <bekzodsanakulov89@gmail.com>
2021-09-14 11:59:05 +0900

    fix: change env ports into 4007, disable cors and deployment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    console.log('rest api - get')
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule , {cors: true});
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 4007);
 }
 bootstrap();
  
